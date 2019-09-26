ac04501ccc435a425f38bf5fc0594de75f648384
Sean <sean.cowan1@gmail.com>
2019-09-25 20:26:50 -0400

    moved location of script includes in index.html, added function to index.js to add items to list

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 $(function () {
-    
+    $('#js-shopping-list-form').submit(e => {
+        e.preventDefault();
+        $('#shopping-list-entry').empty();
+
+        const inputValue = $(this).find('input[name="shopping-list-entry"]').val();
+        const output = `<li><span class="shopping-item">${inputValue}</span><div class="shopping-item-controls">
+          <button class="shopping-item-toggle"><span class="button-label">check</span></button>
+          <button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>`
+
+        $(".shopping-list").append(output);
+    });
 });
