b307ee5cb235fe21cd6d0abb5adeb32ccc55cf67
Sean <sean.cowan1@gmail.com>
2019-09-25 21:57:46 -0400

    added check/delete functions, added a bit of validation on the output

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 $(function () {
     $('#js-shopping-list-form').submit(e => {
         e.preventDefault();
-        $('#shopping-list-entry').empty();
+        const isEmpty = $('#shopping-list-entry').val();
+        if(isEmpty != '') {
+            const inputValue = $(this).find('input[name="shopping-list-entry"]').val();
+            const output = `<li><span class="shopping-item">${inputValue}</span><div class="shopping-item-controls">
+            <button class="shopping-item-toggle"><span class="button-label">check</span></button>
+            <button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>`
+            $('#shopping-list-entry').val('');
+            $('.shopping-list').append(output);
+        }
+    });
 
-        const inputValue = $(this).find('input[name="shopping-list-entry"]').val();
-        const output = `<li><span class="shopping-item">${inputValue}</span><div class="shopping-item-controls">
-          <button class="shopping-item-toggle"><span class="button-label">check</span></button>
-          <button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>`
+    $('.shopping-list').on('click', '.shopping-item-toggle', function(e) {
+        $(this).parents('li').find('.shopping-item').toggleClass('shopping-item__checked');
+    });
 
-        $(".shopping-list").append(output);
+    $('.shopping-list').on('click', '.shopping-item-delete', function(e) {
+        $(this).parents('li').detach();
     });
 });
